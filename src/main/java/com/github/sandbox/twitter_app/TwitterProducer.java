df62a882e0adc366efdfc84ebcc4d3fb868045d8
maiorovi <maiorov.prog@gmail.com>
2019-06-10 17:11:31 +0300

    some improvements

--- src/main/java/com/github/sandbox/twitter_app/TwitterProducer.java
+++ src/main/java/com/github/sandbox/twitter_app/TwitterProducer.java
@@ -10,10 +10,13 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -40,6 +43,16 @@ public class TwitterProducer {
         twitterClient.connect();
         LOG.info("Connected to application");
 
+        KafkaProducer<String, String> kafkaProducer = createKafkaProducer();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOG.info("Stopping application");
+            LOG.info("Shutting down client");
+            twitterClient.stop();
+            LOG.info("Stopping kafka producer");
+            kafkaProducer.close();
+        }));
+
         while (!twitterClient.isDone()) {
             String msg = null;
             try {
@@ -51,10 +64,38 @@ public class TwitterProducer {
 
             if (msg != null) {
                 LOG.info("Received message {}", msg);
+                kafkaProducer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if (exception != null) {
+                            LOG.error("Something bad happened ", exception);
+                        }
+                    }
+                });
             }
         }
 
         LOG.info("End of application");
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        String boostrapServers = "localhost:9092";
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //create safe producer
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        //tunning kafka producer, high throughput producer in expense of cpu usage
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 32 * 1024); // 32 kbytes
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, "20"); //ms
+
+        return new KafkaProducer<>(properties);
     }
 
     public Client createTwitterClient(BlockingQueue<String> msgQueue ) {
